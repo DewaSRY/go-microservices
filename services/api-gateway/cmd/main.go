98fc89d54a28c3e017ba666737132bfc69434693
DewaSRY <sdewa6645@gmail.com>
2025-09-26 06:46:16 +0800

    add gracefullt shoutdown

--- services/api-gateway/cmd/main.go
+++ services/api-gateway/cmd/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	httpHandler "DewaSRY/go-microservices/services/api-gateway/internal/infrastructure/http"
 	"DewaSRY/go-microservices/shared/env"
@@ -11,7 +16,7 @@ import (
 
 var (
 	serviceName = "API_GATEWAY"
-	httpAddr    = env.GetString("HTTP_ADDR", "8081")
+	PORT        = env.GetString("PORT", "8081")
 )
 
 func main() {
@@ -24,10 +29,29 @@ func main() {
 	mux.HandleFunc("GET /", handler.GetHealthCheck)
 	mux.HandleFunc("POST /trip/preview", handler.PostTripPreview)
 
-	//RUN SERVER
-	log.Printf("success_run:%s", serviceName)
-	if err := http.ListenAndServe(fmt.Sprintf(":%s", httpAddr), mux); err != nil {
-		log.Printf("%s", fmt.Sprintf("failed_to_run:%s\n", serviceName))
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%s", PORT),
+		Handler: mux,
 	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatalf("failed_to_run:%s", serviceName)
+		}
+	}()
+
+	quite := make(chan os.Signal, 1)
+	signal.Notify(quite, syscall.SIGINT, syscall.SIGALRM)
+	<-quite
+	log.Println("shout down the server")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Println("failed_to_shout_down")
+		server.Close()
+	}
+
+	log.Println("gracefully_Shout_down")
 
 }
